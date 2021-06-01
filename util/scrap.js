d2717da0531835164266b1252c4eae0d91c77f75
Volas171 <admin@o7fire.tk>
2021-06-01 13:05:39 -0500

    socks5

--- util/scrap.js
+++ util/scrap.js
@@ -3,7 +3,7 @@ const request = require('request');
 var scrapeProxies = new Promise((resolve, reject) => {
     request({
         method: "GET",
-        url: "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+        url: "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all",
     }, (error, response, body) => {
         if (body) {
             return resolve(body);

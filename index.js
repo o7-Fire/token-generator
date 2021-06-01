3da3efb4d224ff0c51bcd8bf2821cf60dd15eb2c
Volas171 <admin@o7fire.tk>
2021-06-01 13:10:11 -0500

    yet im still retarded

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const proxyChecker = require('proxy-checker');
+const { scrapeProxies } = require('./util/scraper.js');
 var anticaptcha = require('./anticaptcha')('tokenhere'); //<<< your anticaptcha token
 
 var fs = require('fs');
-anticaptcha.setWebsiteURL("https://discordapp.com/register");
+var proxies = [];
+
+anticaptcha.setWebsiteURL("https://discord.com/register");
 anticaptcha.setWebsiteKey("6Lef5iQTAAAAAKeIvIY-DeexoO3gj7ryl9rLMEnn");
-anticaptcha.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116");
+anticaptcha.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36");
+
+
 anticaptcha.getBalance(function (err, balance) {
     if (err) {
         console.error(err);
@@ -31,4 +37,27 @@ anticaptcha.getBalance(function (err, balance) {
             });
         });
     }
+	//snig
+scrapeProxies.then(fetched => {
+    fs.writeFile('util/proxies.txt', fetched, (err) => {
+        if (err) throw err;
+        proxyChecker.checkProxiesFromFile('Source/proxies.txt', {
+            url: 'https://discord.com',
+            regex: /It's time to ditch Skype and TeamSpeak./
+        }, (host, port, ok, statusCode, err) => {
+            if (ok) proxies.push(`${host}:${port}`);
+        });
+    });
+    console.log(`Checking ${fetched.length} proxies!`);
+}).catch(err => {
+    proxyChecker.checkProxiesFromFile('util/proxies.txt', {
+        url: 'https://discord.com',
+        regex: /It's time to ditch Skype and TeamSpeak./
+    }, (host, port, ok, statusCode, err) => {
+        if (ok) proxies.push(`${host}:${port}`);
+    });
+    console.log(`Checking proxies from proxies.txt!`);
 });
+
+});
+

b63571f23cc5e11bf9e136eea78d415f648eae42
Volas171 <admin@o7fire.tk>
2021-06-01 13:04:33 -0500

    lol lmao

---
+++ util/scrap.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+var scrapeProxies = new Promise((resolve, reject) => {
+    request({
+        method: "GET",
+        url: "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+    }, (error, response, body) => {
+        if (body) {
+            return resolve(body);
+        } else {
+            return reject();
+        }
+    });
+});
+
+module.exports = { scrapeProxies };
